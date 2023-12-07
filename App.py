7779e3518e692e2cc3033dcfe8c1ee00cf57953e
Kesia Pereira <kesiapereira2003@gmail.com>
2023-12-07 17:00:13 -0300

    fix: Melhorias na organizacao.

--- App.py
+++ App.py
@@ -3,19 +3,19 @@ import matplotlib.pyplot as plt
 import numpy as np
 import math
 
-#Gerar um plano cartesiano vazio.
-def gerar_plano_cartesiano():
-    x = 0
-    y = 0
-
+#Configurando o previamente o Plano Cartesiano.
+def configurar_plano_cartesiano():
     ax = plt.axes()
-    ax.set_xlim([-10, 10]) #Limite X do plano cartesiano.
-    ax.set_ylim([-10, 10]) #Limite Y do plano cartesiano.
-    ax.quiver(0, 0, x, y, angles="xy", scale_units="xy", scale=1, color="red")
+    ax.set_xlim([-10, 10])
+    ax.set_ylim([-10, 10])
     ax.grid()
-
     plt.plot([-10, 10], [0, 0], color="gray")
     plt.plot([0, 0], [-10, 10], color="gray")
+
+#Gerar um plano cartesiano vazio.
+def gerar_plano_cartesiano():
+    configurar_plano_cartesiano()
+    plt.quiver(0, 0, 0, 0, angles="xy", scale_units="xy", scale=1, color="red")
     plt.show()
     return "Fim do exemplo."
 
@@ -35,40 +35,52 @@ def gerador_de_vetores(a, b, c):
     plt.show()
     return "Fim da exibição."
 
+#Funcao para calcular distancia.
+def calcular_distancia(ponto1, ponto2):
+    dx = ponto1[0] - ponto2[0]
+    dy = ponto1[1] - ponto2[1]
+    return math.sqrt(dx**2 + dy**2)
+
+#Funcao para calcular o campo da carga.
+def calcular_campo_carga(carga, posicao, ponto_p):
+    dx = ponto_p[0] - posicao[0]
+    dy = ponto_p[1] - posicao[1]
+    distancia = calcular_distancia(ponto_p, posicao)
+
+    campo_x = (9 * carga * dx) / distancia**3
+    campo_y = (9 * carga * dy) / distancia**3
+
+    return campo_x, campo_y
+
 #Realizando os calculos.
 def calcular_campo_eletrico(cargas, posicoes, ponto_p):
-    campo_total = [0, 0]  # Inicializa o campo elétrico total como zero
+    campo_total = [0, 0]
 
     for carga, posicao in zip(cargas, posicoes):
-        dx = ponto_p[0] - posicao[0]
-        dy = ponto_p[1] - posicao[1]
-        distancia = math.sqrt(dx**2 + dy**2)
-
-        campo_x = (9 * carga * dx) / distancia**3
-        campo_y = (9 * carga * dy) / distancia**3
-
-        campo_total[0] += campo_x
-        campo_total[1] += campo_y
+        campo_carga = calcular_campo_carga(carga, posicao, ponto_p)
+        campo_total[0] += campo_carga[0]
+        campo_total[1] += campo_carga[1]
 
     print("O Campo Elétrico no ponto P é igual a:")
     return campo_total
 
 
 #Introdução
-input("Olá! Para inciarmos nossos cálculos, vamos utilizar de um plano cartesiano.\nPRESSIONE ENTER PARA PROSSEGUIR COM O EXEMPLO.")
+print("Olá! Vamos calcular o Campo Elétrico em um ponto P.\nPrimeiramente, começamos com um Plano Cartesiano Vazio:")
+input("Pressione ENTER para continuar.")
 print(gerar_plano_cartesiano())
 
-print("Agora, seguiremos com os valores para dar início aos cálculos.\nINFORME O VALOR DOS PONTOS A SEGUIR.")
+print("Agora, seguiremos com os valores para dar início aos cálculos.\nInforme o valor dos pontos a seguir:")
 
 #Definindo as coordenadas de cada ponto.
 x1 = float(input("Insira o valor de x no 1º ponto:\n"))
 y1 = float(input("Insira o valor de y no 1º ponto:\n"))
 vetor1 = [x1, y1]
-x2 = float(input("Insira o valor de x no 2º ponto::\n"))
-y2 = float(input("Insira o valor de y no 2º ponto::\n"))
+x2 = float(input("Insira o valor de x no 2º ponto:\n"))
+y2 = float(input("Insira o valor de y no 2º ponto:\n"))
 vetor2 = [x2, y2]
-x3 = float(input("Insira o valor de x no 3º ponto::\n"))
-y3 = float(input("Insira o valor de y no 3º ponto::\n"))
+x3 = float(input("Insira o valor de x no 3º ponto:\n"))
+y3 = float(input("Insira o valor de y no 3º ponto:\n"))
 vetor3 = [x3, y3]
 
 #Chamando o plano cartesiano com os vetores.
@@ -87,9 +99,11 @@ xp = float(input("Insira o valor X do ponto P:\n"))
 yp = float(input("Insira o valor Y do ponto P:\n"))
 
 #Juntando todos os dados.
-cs = [c1, c2, c3]
-pontos = [vetor1, vetor2, vetor3]
-p = [xp, yp]
+cs = [float(input(f"Insira o valor da carga {i + 1}:\n")) for i in range(3)]
+pontos = [[float(input(f"Insira o valor de x no {i + 1}º ponto:\n")),
+           float(input(f"Insira o valor de y no {i + 1}º ponto:\n"))] for i in range(3)]
+p = [float(input("Insira o valor X do ponto P:\n")), float(input("Insira o valor Y do ponto P:\n"))]
+
 
 print(calcular_campo_eletrico(cs, pontos, p))
 print("Fim do programa!")
